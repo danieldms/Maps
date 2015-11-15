60062533d8d6c89894a4d080ebae4c89d2ac97a0
daniel marques <daniel.dms.23@gmail.com>
2015-11-15 12:19:31 -0300

    Set User location
    GEOLOCATION for user

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -7,12 +7,83 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Location user
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.requestAlwaysAuthorization()
+        
+        
+        // Updating maps (Location user)
+        locationManager.startUpdatingLocation()
+        
+        
+        let latitude:CLLocationDegrees  = -23.545384
+        let longitude: CLLocationDegrees = -46.724274
+        
+        let latDelta = 0.01
+        let longDelta = 0.01
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta,longDelta)
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        map.setRegion(region, animated: true)
+        
+        let pressureManager = UILongPressGestureRecognizer(target: self, action: "action:")
+        pressureManager.minimumPressDuration = 2
+        
+        map.addGestureRecognizer(pressureManager)
+        map.showsUserLocation = true
+    }
+    
+    func action(gestureRecognizer: UIGestureRecognizer){
+        
+        let touchPoint = gestureRecognizer.locationInView(self.map)
+        
+        let newCoordinate:CLLocationCoordinate2D = map.convertPoint(touchPoint, toCoordinateFromView: self.map)
+        
+        let annotation = MKPointAnnotation();
+        annotation.coordinate = newCoordinate
+        
+        annotation.title = "Lugar Legal"
+        annotation.subtitle = "Não Deixar de ir"
+        
+        self.map.addAnnotation(annotation)
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation:CLLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        
+        let latDelta = 0.01
+        let longDelta = 0.01
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta,longDelta)
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        self.map.setRegion(region, animated: true)
+
+        
     }
 
     override func didReceiveMemoryWarning() {
